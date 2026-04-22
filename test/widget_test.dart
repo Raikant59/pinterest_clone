2a6bc018a359e1a72526fe2c11b30535fc631b44
Raikant Chaudhary <rakantkumar1234@gmail.com>
2026-04-22 16:04:52 +0530

    debug changes for splash

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:pinterest_clone/app.dart';
 import 'package:pinterest_clone/main.dart';
 
 void main() {
