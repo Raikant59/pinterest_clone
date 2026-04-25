e1607ec60c8be83861a30f64b93edaffdff4c156
Raikant Chaudhary <rakantkumar1234@gmail.com>
2026-04-25 22:19:02 +0530

    Remove unwanted code

--- lib/features/search/model/search_board_item.dart
+++ lib/features/search/model/search_board_item.dart
@@ -1,18 +1,3 @@
-// class SearchBoardItem {
-//   final int id;
-//   final String imageUrl;
-//   final String title;
-//   final String subtitle;
-//   final String meta;
-//
-//   const SearchBoardItem({
-//     required this.id,
-//     required this.imageUrl,
-//     required this.title,
-//     required this.subtitle,
-//     required this.meta,
-//   });
-// }
 class SearchBoardItem {
   final int id;
   final String imageUrl;
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,11 +1,3 @@
-// class AppRoutes {
-//   static const String loader = '/loader';
-//   static const String emailEntry = '/auth/email';
-//   static const String login = '/auth/login';
-//   static const String createPassword = '/auth/create-password';
-//   static const String createName = '/auth/create-name';
-//   static const String home = '/home';
-// }
 class AppRoutes {
   static const String loader = '/loader';
   static const String emailEntry = '/auth/email';
